8e8744f920b13f7966432232c12c6ca125d1a17d
陈新坤 <2023295451@qq.com>
2019-08-12 17:43:36 +0800

    stem62s_v0.0.2

--- PS2X.ts
+++ PS2X.ts
@@ -1,25 +1,25 @@
 //% weight=20 color=#436EEE icon="\uf108"
 namespace PS2X_PAD{
 
-    //% blockId=LCD_Init
+    //% blockId=PS2X_PAD_Init
     //% blockGap=8
-    //% block="LCD1IN8 Init"
+    //% block="PS2X_PAD_Init"
     //% shim=PS2X_PAD::PS2X_PAD_Init
     //% weight=200
     export function PS2X_PAD_Init(): void {
         return;
     }
 
-    //% blockId=LCD_Clear
+    //% blockId=PS2X_PAD_Scan
     //% blockGap=8
-    //% block="Clear screen and cache"
+    //% block="PS2X_PAD_Scan"
     //% shim=PS2X_PAD::PS2X_PAD_Scan
     //% weight=195
     export function PS2X_PAD_Scan(): void {
         return;
     }
 
-    //% blockId=LCD_Filling
+    //% blockId=PS2X_Key
     //% blockGap=8
     //% block="按键 %button"
     //% shim=PS2X_PAD::PS2X_Key
